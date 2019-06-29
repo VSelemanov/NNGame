370c461358f766d50397e4bcf678a2ac30d355c6
Влад Селеманов <selemanovvo@lad24.ru>
2019-06-29 08:27:49 +0300

    Билд фронта, докер контейнер

---
+++ frontend/build/precache-manifest.f6b7ba1004ef14f096f24a4ed778c0a1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d2ea39c5f7f21e9f078a9ab8841e44c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5efe9874c67b7feb143f",
+    "url": "/static/css/main.95eb1882.chunk.css"
+  },
+  {
+    "revision": "df48d410590d4ebe5792",
+    "url": "/static/js/2.b8e8ae88.chunk.js"
+  },
+  {
+    "revision": "5efe9874c67b7feb143f",
+    "url": "/static/js/main.43aaedc7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b464757538cc558f1ebc604dc962d68f",
+    "url": "/static/media/commandNameInput.b4647575.svg"
+  },
+  {
+    "revision": "6fb21958a645b0ee8d5c47a827ae37e8",
+    "url": "/static/media/custom_button.6fb21958.svg"
+  },
+  {
+    "revision": "02aca5a53110ea6b89f07b46045d8a7f",
+    "url": "/static/media/keboard_bg.02aca5a5.svg"
+  },
+  {
+    "revision": "1215309d43fa0fc3fb9ec11d5ad0c521",
+    "url": "/static/media/map_bg.1215309d.svg"
+  },
+  {
+    "revision": "d126bc1bef7a5a3077816a7844e5f6b8",
+    "url": "/static/media/preslav.d126bc1b.otf"
+  },
+  {
+    "revision": "30cef0eae6196e3a6743650a66a63d92",
+    "url": "/static/media/pt-serif.30cef0ea.ttf"
+  },
+  {
+    "revision": "24540f2fae1ef0bc797e9da3b9fe0514",
+    "url": "/static/media/red-name.24540f2f.svg"
+  },
+  {
+    "revision": "1ebcba312731caf99929feced8f8a626",
+    "url": "/static/media/result_team1_bg.1ebcba31.svg"
+  },
+  {
+    "revision": "f0ef26f8b065296ceffb27e09ffab7a4",
+    "url": "/static/media/result_team2_bg.f0ef26f8.svg"
+  },
+  {
+    "revision": "8e7a18ed408832ef02acc4de9bc50328",
+    "url": "/static/media/result_team3_bg.8e7a18ed.svg"
+  }
+]);
