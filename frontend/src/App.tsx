abebc3920de456c573f2729577d0d99b2c8c8b1f
ShipulinIA <ShipulinIA@lad24.ru>
2019-07-16 09:13:30 +0300

    add team info display on left part map

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { Routing } from './Routing';
-import store from './store';
-import { push } from 'connected-react-router';
-import { methodsCookie } from './exports_func';
 
 class App extends React.Component {
   // public componentWillMount() {
--- frontend/src/components/Map/Map.tsx
+++ frontend/src/components/Map/Map.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import style from './Map.module.scss';
-// import { Link } from 'react-router-dom';
-// import map from '../../img/map.svg';
 import MapVector from './MapVector';
 import { createRoom, startGame, getQuestion } from '../../toServer/requests';
 import { connect } from 'react-redux';
@@ -47,13 +45,16 @@ class Map extends React.Component<any, any> {
     const client = new Nes.Client("ws://188.68.210.120:3000");
     const start = async () => {
       await client.connect({ auth: { headers: { authorization: `Bearer ${store.getState().global.appToken}` } } });;
-      const handler = (update: any, flags: any) => {
-        console.log(update, flags)
+      const handler = (message: any, flags: any) => {
+        console.log(message)
+        if(message.teams && Object.keys(this.state.teams).length === 0){
+          this.setState({
+            teams: message.teams
+          })
+        }
       };
-
       client.subscribe("/api/room/gamestatus", handler);
     };
-
     start();
 }
 
@@ -63,16 +64,16 @@ class Map extends React.Component<any, any> {
 			<div className={style.main}>
 				<div className={style.left_panel}>
 					<div className={style.command_info}>
-						<span>{teams.team1 ? teams.team1.name : 'Ожидание игрока'}</span>
-						<p>Областей: {this.getTotal('team1')}</p>
+						<span>{teams.team1 ? teams.team1.name : 'Ожидание команды'}</span>
+						<p>Областей: {teams.team1 ? teams.team1.zones : '-'}</p>
 					</div>
 					<div className={style.command_info}>
-						<span>{teams.team2 ? teams.team2.name : 'Ожидание игрока'}</span>
-						<p>Областей: {this.getTotal('team2')}</p>
+						<span>{teams.team2 ? teams.team2.name : 'Ожидание команды'}</span>
+						<p>Областей: {teams.team2 ? teams.team2.zones : '-'}</p>
 					</div>
 					<div className={style.command_info}>
-						<span>{teams.team3 ? teams.team3.name : 'Ожидание игрока'}</span>
-						<p>Областей: {this.getTotal('team3')}</p>
+						<span>{teams.team3 ? teams.team3.name : 'Ожидание команды'}</span>
+						<p>Областей: {teams.team3 ? teams.team3.zones : '-'}</p>
 					</div>
 				</div>
 				<div className={style.command_info_color}>
@@ -82,21 +83,21 @@ class Map extends React.Component<any, any> {
 				</div>
 				<div className={style.right_panel}>
 					<div className={style.button_div}>
-						<button className={style.next_question}>Создать команду</button>
-						<button className={style.game_status}>Создать комнату</button>
+						<button className={style.next_question} onClick={()=>this.setState({isModalTeam: true})}>Создать команду</button>
+						<button className={style.next_question} >Создать комнату</button>
 						<button className={style.next_question}>Старт Игры</button>
 						<button className={style.next_question} onClick={() => getQuestion('numeric')}>
 							Следующий вопрос
 						</button>
 					</div>
-					{/* <KeyboardWindowAdmin
+					{this.state.isNumQuestionModal && <KeyboardWindowAdmin
                   teams={this.state.teams}
                   part1={this.state.part1}
                   question={this.state.question.title}
                   isTimerStarted={this.state.isTimerStarted}
                   answer={this.state.question.numericAnswer}
-                /> */}
-
+                />
+          }
 					<div className={style.map_wrapper}>
 						<MapVector teams={this.state.teams} />
 					</div>
