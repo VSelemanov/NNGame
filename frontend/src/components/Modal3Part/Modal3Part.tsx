0a61c2d4fbd00e7427cbe6e14b2987992a356388
Igor <booger1@yandex.ru>
2019-08-15 20:14:23 +0300

    FIXED BLEADDDDD modalPart3&numPart2

--- frontend/src/components/Modal3Part/Modal3Part.tsx
+++ frontend/src/components/Modal3Part/Modal3Part.tsx
@@ -3,29 +3,7 @@ import style from "./Modal3Part.module.scss";
 import { startTimer } from "../../toServer/requests";
 import img from "../../img/awaiting_clock.svg";
 
-// part3:
-// isStarted: true
-// question:
-// answers: []
-// cAt: "2019-08-09T14:56:31.749Z"
-// count: 2
-// isNumeric: true
-// numericAnswer: 15
-// title: "Через сколько субъектов федерации протекает река Волга?"
-// uAt: "2019-08-15T15:37:11.786Z"
-// _id: "d73da344-5096-42b7-ad2d-6f4d57a5c167"
-// __proto__: Object
-// responses:
-// team1: {response: 523, timer: 18885, result: 2, _id: "31cda453-299d-4548-8716-14db03c49a50"}
-// team2: {response: 525, timer: 21512, result: 1}
-// team3: {response: null, timer: null, result: null, _id: "7b238f2b-24f4-4745-baf3-118e8bdb37a2"}
-// _id: "a36fa2c2-bd86-429a-b0a8-c637ab95245c"
-// __proto__: Object
-// teams: (2) ["team1", "team2"]
-// __proto__: Object
-// teams: {team1: {…}, team2: {…}, team3: {…}}
-// _id: "4ccf5d25-7a81-4248-a3a5-ab727878cfef"
-// __proto__: Object
+
 class Modal3Part extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
@@ -35,8 +13,9 @@ class Modal3Part extends React.Component<any, any> {
   }
 
   public getStyle(responses: any, team: string) {
-    const keys = responses ? Object.keys(responses).filter(key => key !== "_id") : []
-    const isAnswer =  keys.filter((item: any) => responses[item].timer).length === keys.length;
+    const teams = this.props.part3.teams;
+    const keys = responses ? Object.keys(responses).filter(key => key !== "_id" ) : [];
+    const isAnswer =  keys.filter((item: any) => responses[item].timer !== null).length === teams.length;
     if (isAnswer) {
       switch (responses[team].result) {
         case 2:
